c0f3fbc4242f708a194fb9980b51d5d64b2560fe
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-07-04 19:30:28 -0700

    improve http-logger

--- app/src/main/log/http-logger.ts
+++ app/src/main/log/http-logger.ts
@@ -1,13 +1,28 @@
 import chalk from "chalk";
 import responseTime from "response-time";
 
+const TOTAL_URL_SPACE = 35;
+function getFormattedUrl(baseUrl: string, url: string){
+  const fullUrl = baseUrl + url;
+  const fullUrlLength = fullUrl.length;
+
+  const spacesToAdd = TOTAL_URL_SPACE - fullUrlLength
+  const spacesToAddPositive = spacesToAdd >= 0 ? spacesToAdd : 0;
+  const spacesString = new Array(spacesToAddPositive).join(" ");
+  const formattedUrl = fullUrl + spacesString;
+
+  return formattedUrl;
+}
+
+// prettier-ignore
 function getColorfulMethod(method: string) {
   switch(method) {
-    case "GET": return chalk.green(method);
-    case "POST": return chalk.yellow(method);
-    case "PUT": return chalk.blue(method);
-    case "PATCH": return chalk.white(method);
-    case "DELETE": return chalk.red(method);
+    case "GET":     return chalk.green("GET   ");
+    case "POST":    return chalk.yellow("POST  ");
+    case "PUT":     return chalk.blue("PUT   ");
+    case "PATCH":   return chalk.white("PATCH ");
+    case "DELETE":  return chalk.red("DELETE");
+    default:        return chalk.white(method);
   }
 }
 
@@ -18,19 +33,19 @@ function getFormattedTime(time: number) {
   const roundedTimeStrLen = roundedTimeStr.length;
 
   switch (roundedTimeStrLen) {
-    case 1: return `  ${roundedTimeStr} ms`;
-    case 2: return ` ${roundedTimeStr} ms`;
-    default: return `${roundedTimeStr} ms`;
+    case 1:   return `  ${roundedTimeStr} ms`;
+    case 2:   return ` ${roundedTimeStr} ms`;
+    default:  return `${roundedTimeStr} ms`;
   }
 }
 
 function getHttpBlock(res: any) {
-  if (res.statusCode <= 399) return chalk.greenBright("HTTP");
+  if (res.statusCode <= 399) return chalk.green("HTTP");
   return chalk.redBright("HTTP");
 }
 
 function getStatusCode(res: any) {
-  if (res.statusCode <= 399) return chalk.greenBright(res.statusCode);
+  if (res.statusCode <= 399) return chalk.green(res.statusCode);
   return chalk.redBright(res.statusCode);
 }
 
@@ -42,7 +57,8 @@ const HttpLogger = responseTime((req: any, res: any, time) => {
   const formattedTime = getFormattedTime(time);
 
   const colorfulMethod = getColorfulMethod(req.method);
-  let MessageBlock = `${colorfulMethod} ${req.baseUrl + req.url} ${StatusCode}`;
+  const formattedUrl = getFormattedUrl(req.baseUrl, req.url);
+  let MessageBlock = `${colorfulMethod} ${formattedUrl} ${StatusCode}`;
 
   const possibleError = res.locals.errType;
   if (possibleError) MessageBlock += ` --> ${chalk.redBright(possibleError)}`;
