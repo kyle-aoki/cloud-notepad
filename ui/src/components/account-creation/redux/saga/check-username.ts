dc41c053f533b927b055a253d5aa042c82286196
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-07-06 19:39:41 -0700

    Update check-username.ts

--- ui/src/components/account-creation/redux/saga/check-username.ts
+++ ui/src/components/account-creation/redux/saga/check-username.ts
@@ -2,20 +2,18 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import UserAPI from '../../../../api/user-api';
 import { NotificationType } from '../../../../notifications/redux/reducer';
 import { CreateAccountModalAction, CreateAccountModalActions } from '../reducer';
-import { CheckUsernameResponse } from '@cloud-notepad/cloud-notepad-response';
+import { CheckUsernameResponse, ValidationResponse } from '@cloud-notepad/cloud-notepad-response';
 import NotificationControl from '../../../../notifications/redux/control';
 import { AccountCreationControl } from '../control';
 
 // Click 'Next' button.
 function* CheckUsernameSaga(action: CreateAccountModalAction): Generator<any, any, any> {
+  if (!action.payload.username || action.payload.usernameLoading) return;
+
   const NotificationController = new NotificationControl(put);
   const AccountCreationController = new AccountCreationControl(put);
 
-  if (action.payload.usernameLoading) return;
-
   yield AccountCreationController.USERNAME_LOADING();
-
-  if (!action.payload.username) return yield put({ type: CreateAccountModalActions.BAD_USERNAME });
 
   let checkUsernameResult;
   try {
@@ -24,14 +22,26 @@ function* CheckUsernameSaga(action: CreateAccountModalAction): Generator<any, an
     return yield NotificationController.NETWORK_ERROR();
   }
 
-  if (checkUsernameResult.ok) return yield put({ type: CreateAccountModalActions.GO_TO_PASSWORD_SCREEN });
-
-  if (checkUsernameResult.type === CheckUsernameResponse.USER_EXISTS) {
+  if (checkUsernameResult.ok) {
     yield AccountCreationController.STOP_USERNAME_LOADING();
-    yield NotificationController.PUSH_ERROR('That username is taken. Try another.');
+    yield AccountCreationController.GO_TO_PASSWORD_SCREEN();
     return;
   }
-  return yield NotificationController.GENERIC_ERROR();
+
+  // prettier-ignore
+  switch (checkUsernameResult.type) {
+    case CheckUsernameResponse.USER_EXISTS: 
+      yield NotificationController.PUSH_ERROR('That username is taken. Try another.'); break;
+    case ValidationResponse.INVALID_USERNAME_SYMBOLS: 
+      yield NotificationController.PUSH_ERROR('Invalid symbols in username. Can only use - and _ in username.'); break;
+    case ValidationResponse.USERNAME_SHORT: 
+      yield NotificationController.PUSH_ERROR('That username is too short.'); break;
+    case ValidationResponse.USERNAME_LONG: 
+      yield NotificationController.PUSH_ERROR('That username is too long.'); break;
+    default: 
+      yield NotificationController.GENERIC_ERROR();
+  }
+  yield AccountCreationController.STOP_USERNAME_LOADING();
 }
 
 export function* CheckUsernameSagaMiddleware() {
