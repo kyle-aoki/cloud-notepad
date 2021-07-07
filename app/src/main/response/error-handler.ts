4eea462a27b6454a491c2b8b6d648c656e475d84
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-07-06 19:31:10 -0700

    Update error-handler.ts

--- app/src/main/response/error-handler.ts
+++ app/src/main/response/error-handler.ts
@@ -1,6 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import Log from "../log";
-import { inDevelopment, inStaging } from "../utility/environment";
 
 interface ErrorResponseEntity {
   ok: false;
