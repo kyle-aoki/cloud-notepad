28195374366d5c527fd7a81b898b4543183df247
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-07-10 16:32:54 -0700

    whitespace

--- ui/src/components/file-system/file-system.tsx
+++ ui/src/components/file-system/file-system.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { ReactComponent as CloudIcon } from '../../assets/cloud.svg';
-import { Arrow, CloudIconStyle, Header, SubPath, XButtonSVGContainer } from './components';
-import { File, Folder } from './objects';
+import { Arrow, CloudIconStyle, SubPath, XButtonSVGContainer } from './components';
 import { FileSystemControl, useFileSystemState } from './redux/control';
 import {
   ArrowsContainer,
@@ -23,62 +22,59 @@ import {
   ViewContainer,
   XButton,
 } from './styled-components';
-import { getCurrentPath, getPath } from './util';
 
 export default function FileSystem() {
   const FileSystemController = new FileSystemControl(useDispatch());
   const { userDir, path } = useFileSystemState();
 
   return (
-    <>
-      <FileSystemContainer>
-        <TopContainer>
-          <HandleContainer>
-            <Handle>
-              <Title>File Explorer - Open</Title>
-            </Handle>
-            <XButton onClick={() => FileSystemController.CLOSE_FILE_SYSTEM()}>
-              <XButtonSVGContainer />
-            </XButton>
-          </HandleContainer>
-          <Taskbar>
-            <ArrowsContainer>
-              <Arrow onClick={() => FileSystemController.BACK_BUTTON_PRESSED()} />
-              <Arrow right onClick={() => FileSystemController.FORWARD_BUTTON_PRESSED()} />
-            </ArrowsContainer>
-            <PathContainer>
-              <SubPath>
-                <CloudIcon style={CloudIconStyle} />
-              </SubPath>
-              <SubPath>Cloud Storage</SubPath>
-              {path.map((subpath: string, index: number, array: string[]) => {
-                // if (index === 0) return;
-                // if (index === array.length - 1) return subpath.replace('/', '');
-                const last = index === array.length - 1;
-                return <SubPath last={last}>{subpath}</SubPath>;
-              })}
-            </PathContainer>
-            <StorageCapacity>{'0 KB'} / 1000 KB</StorageCapacity>
-          </Taskbar>
-        </TopContainer>
-        <Container>
-          <Sidebar></Sidebar>
-          <ViewContainer>
-            <View></View>
-          </ViewContainer>
-        </Container>
-        <Controller>
-          {/* <ButtonContainer>
+    <FileSystemContainer>
+      <TopContainer>
+        <HandleContainer>
+          <Handle>
+            <Title>File Explorer - Open</Title>
+          </Handle>
+          <XButton onClick={() => FileSystemController.CLOSE_FILE_SYSTEM()}>
+            <XButtonSVGContainer />
+          </XButton>
+        </HandleContainer>
+        <Taskbar>
+          <ArrowsContainer>
+            <Arrow onClick={() => FileSystemController.BACK_BUTTON_PRESSED()} />
+            <Arrow right onClick={() => FileSystemController.FORWARD_BUTTON_PRESSED()} />
+          </ArrowsContainer>
+          <PathContainer>
+            <SubPath>
+              <CloudIcon style={CloudIconStyle} />
+            </SubPath>
+            <SubPath>Cloud Storage</SubPath>
+            {path.map((subpath: string, index: number, array: string[]) => {
+              // if (index === 0) return;
+              // if (index === array.length - 1) return subpath.replace('/', '');
+              const last = index === array.length - 1;
+              return <SubPath last={last}>{subpath}</SubPath>;
+            })}
+          </PathContainer>
+          <StorageCapacity>{'0 KB'} / 1000 KB</StorageCapacity>
+        </Taskbar>
+      </TopContainer>
+      <Container>
+        <Sidebar></Sidebar>
+        <ViewContainer>
+          <View></View>
+        </ViewContainer>
+      </Container>
+      <Controller>
+        {/* <ButtonContainer>
             <Button>Save</Button>
             <Button>Cancel</Button>
           </ButtonContainer> */}
-          <Button>Delete</Button>
-          <ButtonContainer>
-            <Button>Move</Button>
-            <Button>Open</Button>
-          </ButtonContainer>
-        </Controller>
-      </FileSystemContainer>
-    </>
+        <Button>Delete</Button>
+        <ButtonContainer>
+          <Button>Move</Button>
+          <Button>Open</Button>
+        </ButtonContainer>
+      </Controller>
+    </FileSystemContainer>
   );
 }
