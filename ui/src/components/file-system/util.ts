8f162116c355244ff79cdee555b8b4c506999659
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-07-23 21:27:54 -0700

    Update util.ts

--- ui/src/components/file-system/util.ts
+++ ui/src/components/file-system/util.ts
@@ -1,6 +1,3 @@
-export const getCurrentPath = (path: string[]) => path.join('/');
-export const getPath = (pathArray: string[]) => pathArray.join('/');
-
 const TOTAL_MEMORY_LOCAL_STORAGE_KEY = 'totalMemory' as const;
 
 export function getTotalMemoryFromLocalStorage() {
@@ -10,6 +7,10 @@ export function getTotalMemoryFromLocalStorage() {
     return 0;
   }
 }
+
 export function setTotalMemoryInLocalStorage(totalMemory: number) {
   localStorage.setItem(TOTAL_MEMORY_LOCAL_STORAGE_KEY, totalMemory.toString());
 }
+
+export const getCurrentPath = (path: string[]) => path.join('/');
+export const getPath = (pathArray: string[]) => pathArray.join('/');
